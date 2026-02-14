0a75fda8b7b0a048a0df243f9fc438429215bb46
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2026-02-14 09:23:34 +0800

    feat: add support for OOP

---
+++ oop/class.lua
@@ -0,0 +1,13 @@
+-- class definition
+class = {
+    new = function(self, tbl)
+        tbl = tbl or {}
+        setmetatable(tbl, { __index = self })
+        return tbl
+    end
+}
+
+entity = class:new({
+    x = 0,
+    y = 0
+})
