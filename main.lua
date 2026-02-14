42ba7ffc30d6997e53f0e7aa7acb3dff5c2b2570
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2026-02-14 09:23:58 +0800

    feat: add game manager class

---
+++ main.lua
@@ -0,0 +1,129 @@
+-- game state
+game_states = {
+    start = "start",
+    serve = "serve",
+    play = "play",
+    done = "done"
+}
+game = entity:new({
+    score = 0,
+    screen_boundary = 128,
+    build = function(self, ball, player, enemy)
+        self.ball = ball
+        self.player = player
+        self.enemy = enemy
+
+        self.state = game_states.serve
+        self.ball:init(self.screen_boundary / 2)
+        self.player:init()
+        self.enemy:init()
+    end,
+    update = function(self)
+        self.player:move()
+
+        if self.state == game_states.play then
+            self.enemy:move()
+            self.ball:move()
+            self:check_collisions()
+        elseif self.state == game_states.serve then
+            if self.player.health <= 0 then
+                self.finish_reason = "lose"
+                self.state = game_states.done
+            end
+
+            if self.score >= 5 then
+                self.finish_reason = "win"
+                self.state = game_states.done
+            end
+
+            if btn(❎) then
+                self.state = game_states.play
+            end
+        end
+    end,
+    check_collisions = function(self)
+        ball_boundaries = {
+            left = self.ball.x + self.ball.dx - self.ball.size,
+            right = self.ball.x + self.ball.dx + self.ball.size,
+            top = self.ball.y + self.ball.dy - self.ball.size,
+            bottom = self.ball.y + self.ball.dy + self.ball.size
+        }
+
+        player_paddle_boundaries = {
+            right = self.player.x + self.player.width,
+            top = self.player.y,
+            bottom = self.player.y + self.player.height
+        }
+
+        enemy_paddle_boundaries = {
+            left = self.enemy.x,
+            top = self.enemy.y,
+            bottom = self.enemy.y + self.enemy.height
+        }
+
+        -- ball collision with player paddle
+        if ball_boundaries.left <= player_paddle_boundaries.right
+                and ball_boundaries.bottom >= player_paddle_boundaries.top
+                and ball_boundaries.top <= player_paddle_boundaries.bottom then
+            -- printh("ball_boundaries.left: " .. ball_boundaries.left, "log", true)
+            -- stop()
+            self.ball:bounce_x()
+        end
+
+        -- ball collision with enemy paddle
+        if ball_boundaries.right >= enemy_paddle_boundaries.left
+                and ball_boundaries.bottom >= enemy_paddle_boundaries.top
+                and ball_boundaries.top <= enemy_paddle_boundaries.bottom then
+            self.score += 1
+            self:reset_elements()
+        end
+
+        -- ball collision with top and bottom screen boundaries
+        if ball_boundaries.top <= 0
+                or ball_boundaries.bottom >= self.screen_boundary then
+            self.ball:bounce_y()
+        end
+
+        -- ball collision with right screen boundary only
+        if ball_boundaries.right >= self.screen_boundary then
+            self.ball:bounce_x()
+        end
+
+        -- ball goes past left screen boundary
+        if ball_boundaries.left <= 0 then
+            -- self.state = game_states.done
+            self.player:decrease_health()
+            self:reset_elements()
+        end
+    end,
+    reset_elements = function(self)
+        self.ball:init(self.screen_boundary / 2)
+        self.player:init()
+        self.enemy:init()
+
+        self.state = game_states.serve
+    end,
+    draw_elements = function(self)
+        if self.state == game_states.done then
+            self:draw_game_over_screen()
+        else
+            self.player:draw()
+            self.enemy:draw()
+            self.ball:draw()
+
+            -- draw score in top right corner of screen
+            print("score: " .. self.score, self.screen_boundary - 40, 5, 7)
+
+            -- draw player health in top left corner of screen
+            print("health: " .. self.player.health, 5, 5, 7)
+        end
+    end,
+    draw_game_over_screen = function(self)
+        cls()
+        if self.finish_reason == "win" then
+            print("you win!", 44, 56, 7)
+        else
+            print("you lose!", 40, 56, 7)
+        end
+    end
+})
