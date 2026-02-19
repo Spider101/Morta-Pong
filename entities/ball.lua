963bd9cf92f50aecd0ed8c018980dbabe0f5f5a5
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2026-02-19 21:43:27 +0800

    feat: use sprite for ball and attach particle trail to it

--- entities/ball.lua
+++ entities/ball.lua
@@ -2,18 +2,24 @@
 ball = entity:new({
     dx = 1.5,
     dy = 2,
-    size = 2,
-    color = 7,
-
+    size = block_size,
+    trail = particle_trail,
     init = function(self, x, y, direction_x, direction_y)
-        self.x = x - (self.size / 2)
+        self.x = x - self.size
         self.y = y - (self.size / 2)
         self.dx = self.dx * direction_x
         self.dy = self.dy * direction_y
+        self.trail:flush()
     end,
     move = function(self)
         self.x += self.dx
         self.y += self.dy
+
+        -- spawn trail particles
+        local ball_radius = self.size / 2
+        local center_x = self.x + ball_radius
+        local center_y = self.y + ball_radius
+        self.trail:spawn(center_x, center_y, ball_radius)
     end,
     bounce_x = function(self)
         self.dx = -self.dx
@@ -22,11 +28,7 @@ ball = entity:new({
         self.dy = -self.dy
     end,
     draw = function(self)
-        circfill(
-            self.x,
-            self.y,
-            self.size,
-            self.color
-        )
-    end
+        spr(23, self.x, self.y)
+        self.trail:draw()
+    end,
 })
